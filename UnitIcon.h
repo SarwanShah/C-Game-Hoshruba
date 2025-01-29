da3409a98bf9f340992286a2851a75570b17a6d1
sarwanshah <sarwanshah1996@gmail.com>
2025-01-28 20:26:06 -0700

    Updating Project Files

--- UnitIcon.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "OnScreenObject.h"
-
-class UnitIcon:public OnScreenTexture
-{
-private:
-public:
-    UnitIcon(LTexture* textureSheet, SDL_Rect* textureRect, int x_pos, int y_pos, int x_size = 0, int y_size = 0);
-};
